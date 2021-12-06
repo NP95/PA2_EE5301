b40c23628b3cb21006118a106f2497e723390a7e
Nishant VM <nishantpani95@gmail.com>
2021-12-06 06:43:28 -0500

    Final node defintions

--- src/CircuitNode_PA2.cpp
+++ src/CircuitNode_PA2.cpp
@@ -5,65 +5,86 @@
 //Consensus seems it is tricky to do so, since private variables, use a setter method
 //These are setter methods
 
-void CircuitNode::setGateArea(const int& area)
+void CircuitNode::set_node_id(const NodeID& node_id) 
 {
-  area = area; 
+    node_id_ = node_id;
 }
 
 void CircuitNode::set_x_coordinate(const int& x_coordinate)
 {
-
-
+   x_coordinate = x_coordinate;
 }
 
 void CircuitNode::set_y_coordinate(const int& y_coordinate)
-{}
-
-void CircuitNode::set_node_id(const NodeID& node_id) {
-    node_id_ = node_id;
+{
+   y_coordinate = y_coordinate;
 }
 
-void CircuitNode::set_input_pad(const bool& input_pad) {
-    input_pad_ = input_pad;
+void CircuitNode::set_gate_area(const int& area)
+{
+  area = area; 
 }
 
-void CircuitNode::set_output_pad(const bool& output_pad) {
-    output_pad_ = output_pad;
+void CircuitNode::set_construction_id(const int& construction_id) 
+{
+    construction_id = construction_id;
 }
 
-void CircuitNode::set_gate_type(const std::string& gate_type) {
+void CircuitNode::set_gate_type(const std::string& gate_type) 
+{
     gate_type_ = gate_type;
 }
 
-
-void CircuitNode::add_to_fanin_list(const NodeID& node_id) {
+void CircuitNode::add_to_fanin_list(const NodeID& node_id) 
+{
     fanin_list_.push_back(node_id);
 }
 
+void CircuitNode::add_to_fanout_list(const NodeID& node_id)
+{
+   fanout_list_.push_back(node_id);
+}
 
 //These are getter methods, the const declaration ensures that calling them does not modify the object
-const NodeID& CircuitNode::get_node_id() const {
+const NodeID& CircuitNode::get_node_id() const 
+{
     return node_id_;
 }
 
-const bool& CircuitNode::is_input_pad() const {
-    return input_pad_;
+
+const int& CircuitNode::get_x_coordinate() const 
+{
+    return x_coordinate;
 }
 
-const bool& CircuitNode::is_output_pad() const {
-    return output_pad_;
+const int& CircuitNode::get_y_coordinate() const
+{
+	return y_coordinate;
 }
 
-const std::string& CircuitNode::get_gate_type() const {
+const GateArea& CircuitNode::get_area() const
+{
+	return area;
+}
+
+const int& CircuitNode::get_construction_id() const
+{
+	return construction_id;
+}
+
+const std::string& CircuitNode::get_gate_type() const 
+{
     return gate_type_;
 }
 
-const GateInfo* CircuitNode::get_gate_info() const {
-    return gate_info_;
-}
 
-const std::list<NodeID>& CircuitNode::get_fanin_list() const {
+const std::list<NodeID>& CircuitNode::get_fanin_list() const 
+{
     return fanin_list_;
 }
 
 
+const std::list<NodeID>& CircuitNode::get_fanout_list() const 
+{
+    return fanout_list_;
+}
--- src/CircuitNode_PA2.hpp
+++ src/CircuitNode_PA2.hpp
@@ -9,34 +9,39 @@ typedef int GateArea;
 class CircuitNode {
     private:
         NodeID node_id_;
-        bool input_pad_;
-        bool output_pad_;
 	int x_coordinate;
 	int y_coordinate;
 	GateArea area;
+	int construction_id;
         std::string gate_type_;
         std::list<NodeID> fanin_list_;
 	std::list<NodeID> fanout_list_;
 
     public:
         CircuitNode() :
-            node_id_(""), input_pad_(false), output_pad_(false),x(0),y(0),area(0)
-            gate_type_("") { }
+            node_id_(""), x_coordinate(0),y_coordinate(0),area(0),construction_id(0),gate_type_("") { }
        //This type of initialization is called the initializer list
        //https://stackoverflow.com/questions/7665021/c-member-initialization-list
        //the order of initialization is decided in the order in which the data members are declared, not the the order in which you have initialized the data members using initialization list
        //https://www.youtube.com/watch?v=1nfuYMXjZsA
         void set_node_id(const NodeID& node_id);
-        void set_input_pad(const bool& input_pad);
-        void set_output_pad(const bool& output_pad);
+	void set_x_coordinate(const int& x_coordinate);
+	void set_y_coordinate(const int& y_coordinate);
+	void set_gate_area(const GateArea& area);
+	void set_construction_id(const int& construction_id);
         void set_gate_type(const std::string& gate_type);
         void add_to_fanin_list(const NodeID& node_id);
+	void add_to_fanout_list(const NodeID& node_id)
+	
        //These are basically function declarations. The definitions are in a separate file 
         const NodeID& get_node_id() const;
-        const bool& is_input_pad() const;
-        const bool& is_output_pad() const;
+        const int& get_x_coordinate() const;
+        const int& get_y_coordinate() const;
+        const GateArea& get_area() const;
+        const int&  get_construction_id() const;
         const std::string& get_gate_type() const;
         const std::list<NodeID>& get_fanin_list() const;
+	const std::list<NodeID>& get_fanout_list() const;
 	/*Explanation copied from here
 	 * https://www.geeksforgeeks.org/const-member-functions-c/
 	 A free function cannot be marked with “const”, only a method can.
